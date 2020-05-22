8ced81de339388e86e14effcdaccdb6bc6e78da4
Jacques <Jacquesokes@gmail.com>
2020-05-22 13:30:26 +0200

    Initial Release

---
+++ Okes.Mapbox.Android.Core/Additions/LastModifiedComparator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Java.Util;
+
+namespace Okes.Mapbox.Android.Core.Additions
+{
+    public partial class LastModifiedComparator 
+    {
+     
+        public int Compare(Java.IO.File a, Java.IO.File b)
+        {
+           return Compare(a, b);
+            
+        }
+             
+    }
+}
